45007dbe1ec4d6e1b86b1194c50bf570fe43eb4a
ottosoderlund <37615363+ottosoderlund@users.noreply.github.com>
2022-09-28 13:52:27 +0300

    pikkumuutoksia

--- pages/index.js
+++ pages/index.js
@@ -12,39 +12,50 @@ export default function Home() {
   const [sent, SetSent] = useState(false)
 
   return (
-    <div className="bg-slate-700 h-screen">
-      <Head>
-        <title>Peräkärrysauna</title>
-        <meta name="description" content="Peräkärrysauna" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      // <div className="bg-gradient-to-r from-rose-400 to-orange-300">
+      <div className="bg-[url('/kuva3-sauna.jpg')] bg-cover bg-center">
+        <Head>
+          <title>Peräkärrysauna</title>
+          <meta name="description" content="Peräkärrysauna" />
+          <link rel="icon" href="/favicon.ico" />
 
-      <main className="h-screen flex flex-col justify-center">
-        <h1 className='text-5xl	text-center	text-zinc-200 font-normal	font-impact'>Osallistu ja voita peräkärrysauna päiväksi!</h1>
-        <div className='flex flex-wrap flex-row justify-center mt-4'>
-          <div className="form-control m-2">
-            <label className="input-group">
-              <span>Sähköposti</span>
-              <input type="text" placeholder="voita@sauna.com" className="input input-bordered" onChange={(x) => SetEmail(x)} />
-            </label>
+        </Head>
+
+        <main className="h-screen flex flex-row justify-center bg-black/30 ">
+          <div className="flex-col flex justify-center">
+            <div className="mx-auto rounded overflow-hidden shadow-lg backdrop-blur-sm justify-center p-10">
+              <div className="font-bold text-xl mb-2 text-gray-100 flex text-center flex justify-center">
+                Olemme rakentamassa!
+              </div>
+              <div className="font-bold text-base mb-2 text-gray-200 flex text-center m-10">Liity odotuslistalle ja voita peräkärrysauna päiväksi!</div>
+
+              <div className='flex flex-wrap flex-row justify-center mt-4'>
+                <div className="form-control m-2">
+                  <label className="input-group">
+                    <span>Sähköposti</span>
+                    <input type="text" placeholder="voita@sauna.com" className="input input-bordered" onChange={(x) => SetEmail(x)} />
+                  </label>
+                </div>
+                <button className="btn btn-primary bg-lime-700 border-lime-700 hover:bg-lime-400 hover:border-lime-400 m-2">Osallistu</button>
+              </div>
+            </div>
+
           </div>
-          <button className="btn btn-primary m-2">Osallistu</button>
-        </div>
+          {/*<img className="w-16 md:w-32 lg:w-48" src="/trailer_puller.svg"/>*/}
 
-
-        <footer className=" absolute  bottom-0 p-7 w-full text-center  text-base-content">
-          <div>
-            <p className='text-zinc-200'>puh. 0400 625 386 | otto.soderlund@normipaivaoy.fi</p>
-          </div>
-        </footer>
+          <footer className=" absolute  bottom-0 p-7 w-full text-center  text-base-content">
+            <div>
+              <p className='text-zinc-200'>puh. 0400 625 386 | saunaan@normipaivaoy.fi</p>
+            </div>
+          </footer>
 
 
 
-      </main>
+        </main>
 
 
 
 
-    </div>
+      </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,16 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    fontFamily: {
-      'impact': "Impact"
-    },
-    extend: {},
-  },
-  plugins: [require("daisyui")],
+    fontSize: {
+      sm: '0.8rem',
+      base: '1.25rem',
+      xl: '2.25rem',
+},
+fontFamily: {
+  'impact': "Impact"
+},
+
+extend: {},
+},
+plugins: [require("daisyui")],
 }
